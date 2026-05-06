9f93e5518dc21410537df64e5c4dee85de6365cb
irishN24 <145177386+irishN24@users.noreply.github.com>
2026-05-06 18:48:21 +0500

    Update second_window.h

--- second_window.h
+++ second_window.h
@@ -2,6 +2,7 @@
 #define SECOND_WINDOW_H
 
 #include <QDialog>
+#include <QDebug>
 
 namespace Ui {
 class Second_window;
@@ -18,10 +19,16 @@ public:
     {
         m_value = m;
     }
+    void setVerificationMatrix(const std::vector<std::vector<int>>& matrix)
+    {
+        H = matrix;
+        qDebug() << "Second_window: verification matrix received, size:" << H.size() << "x" << (H.empty() ? 0 : H[0].size());
+    }
 private:
     Ui::Second_window *ui;
     int m_value = 0;  // параметр m (количество строк)
     int n_value = 0;  // длина кодового слова
+    std::vector<std::vector<int>> H;
 private slots:
     void on_exit_Button_2_clicked();
     void on_back_Button_2_clicked();
